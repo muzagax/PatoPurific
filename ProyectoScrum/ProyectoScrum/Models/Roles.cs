b7ba2bcccecc2f3b9b7ba8e61ed2b48aa4b1afea
sebastianVUCR <sebastian.vargas.99@gmail.com>
2017-10-04 11:34:24 -0600

    mvc roles
    archivos vacíos de modeloRoles, vista roles y controlador roles

--- ProyectoScrum/ProyectoScrum/Models/Roles.cs
+++ ProyectoScrum/ProyectoScrum/Models/Roles.cs
@@ -7,5 +7,12 @@ namespace ProyectoScrum.Models
 {
     public class Roles
     {
+
+        public permisos modeloPermisos { get;  set; }
+        public AspNetRoles modeloNetRoles { get; set; }
+        
+        public List<permisos> listaPermisos { get; set; }
+
+        public List<AspNetRoles> listaRoles { get; set; }
     }
 }
